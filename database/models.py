e1689b4b5ff044f6e9052c99aa8a35501b436edf
Adamya Neupane <adamsneupane@gmail.com>
2024-02-02 22:59:11 +0545

    made blank and null true

--- database/models.py
+++ database/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Transaction(models.Model): 
     Card_Number = models.CharField(max_length = 256)
-    Item_Retrieved = models.CharField(max_length = 256)
-    Number_of_Items = models.IntegerField()
+    Item_Retrieved = models.CharField(max_length = 256, blank=True, null=True)
+    Number_of_Items = models.IntegerField(blank=True, null=True)
     Date_of_Transaction = models.DateField(auto_now_add = True)
     def __str__(self):
         return f"{self.Card_Number} ({self.Date_of_Transaction})"
