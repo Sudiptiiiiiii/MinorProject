6f9c99b2d441732be9ffdb859a852ef9e3341ceb
prashant <prashant.bista.18@gmail.com>
2024-02-29 17:49:59 +0545

    transaction remade

--- database/admin.py
+++ database/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Transaction, Item
+from .models import Transaction, Item,Cards
 
-admin.site.register(Transaction)
-admin.site.register(Item)
+# @admin.register(Transaction)
+# class TransactionAdmin(admin.ModelAdmin):
+#     list_display = ['Date_of_Transaction']
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['Name', 'remaining_number', 'id']
+
+@admin.register(Cards)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['card_number', 'active', 'id']
+
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('card_Number', 'Item_Retrieved', 'Number_of_Items', 'Date_of_Transaction')
+    search_fields = ('card_Number__card_number',)  # Add search functionality for card_number
+
+admin.site.register(Transaction, TransactionAdmin)
